4480effe62f4ca47b247332c96c76f354466a80b
edoalemp <eduardo.munoz.p@gmail.com>
2019-05-31 17:13:38 +0000

    segundo parcial

--- hello-rigo/src/js/component/home.js
+++ hello-rigo/src/js/component/home.js
@@ -1,32 +1,41 @@
 import React from "react";
 import PropType from "prop-types";
 
-class Newtask extends React.Component {
+class Listask extends React.Component {
 	constructor() {
 		super();
 		this.state = {
 			arraytask: [],
 			text: ""
 		};
+		this.createtask = this.createtask.bind(this);
+		this.addtoarraytask = this.addtoarraytask.bind(this);
 	}
 
-	addtoarray(event) {
-		let array = this.arraytask;
+	addtoarraytask(event) {
 		if (event.which == 13) {
+			let array = this.state.arraytask;
 			array.push(event.target.value);
-			this.setState({ arraytask: array });
+			this.setState({
+				arraytask: array
+			});
 		}
+	}
+
+	createtask(event) {
+		this.setState({ text: event.target.value });
+		console.log(this.state);
 	}
 
 	render() {
 		return (
 			<div>
 				<input
-					onKeyPress={this.addtoarray}
+					onKeyPress={this.addtoarraytask}
 					className="form-control form-control-lg"
 					type="text"
 					placeholder="New task"
-					defaultValue={this.state.text}
+					onChange={this.createtask}
 				/>
 				<div>{this.state.arraytask}</div>
 			</div>
@@ -42,7 +51,7 @@ export class Home extends React.Component {
 				<div className="row justify-content-md-center">
 					<div className="col col-lg-6">
 						<div className="text-center mt-5">
-							<Newtask />
+							<Listask />
 						</div>
 					</div>
 				</div>
