32496f6eaa41aa3544f2fe8742d18334b647fb35
edoalemp <eduardo.munoz.p@gmail.com>
2019-06-05 07:17:34 +0000

    inicio

--- hello-rigo/src/js/component/home.js
+++ hello-rigo/src/js/component/home.js
@@ -130,26 +130,3 @@ export class Home extends React.Component {
 		);
 	}
 }
-/*
-fetch("https://assets.breatheco.de/apis/fake/todos/user/alesanchezr", {
-	method: "POST",
-	body: "[]",
-	headers: {
-		"Content-Type": "application/json"
-	}
-})
-	.then(resp => {
-		//console.log(resp.ok); // will be tru if the response is successfull
-		//console.log(resp.status); // the status code = 200 or code = 400 etc.
-		//console.log(resp.text()); // will try return the exact result as string
-		return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
-	})
-	.then(data => {
-		//here is were your code should start after the fetch finishes
-		console.log(data); //this will print on the console the exact object received from the server
-	})
-	.catch(error => {
-		//error handling
-		console.log(error);
-	});
-*/
